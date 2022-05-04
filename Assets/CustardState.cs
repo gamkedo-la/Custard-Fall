f84c433528f6960b517e09b271c68b49b1e9dd14
Kulesko <thegreg@freenet.de>
2022-05-04 22:00:45 +0200

    setup(project): added some initial materials and script skeletons

---
+++ Assets/CustardState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+
+public class CustardState : MonoBehaviour
+{
+    
+    private int[][] custardArea;
+
+    void Start()
+    {
+        custardArea = new int[][]{};
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
