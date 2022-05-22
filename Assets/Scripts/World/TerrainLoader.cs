a57f020e8b0c472f2edcec7273fdd52a542db64d
Kulesko <thegreg@freenet.de>
2022-05-22 19:06:47 +0200

    feature(terrain): load terrain data by scanning the surface at the beginning of the game

---
+++ Assets/Scripts/World/TerrainLoader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Custard;
+using UnityEngine;
+
+namespace World
+{
+    public class TerrainLoader : MonoBehaviour
+    {
+        public WorldCells worldCells;
+        [SerializeField]
+        LayerMask layerMask;
+        
+        public void LoadWorld()
+        {
+            worldCells.Init();
+            ScanTerrain();
+        }
+
+        private void ScanTerrain()
+        {
+            RaycastHit hit;
+            for (byte x = 0; x < WorldCells.BlocksWidth; x++)
+            {
+                for (byte y = 0; y < WorldCells.BlocksHeight; y++)
+                {
+                    var cellMid = CustardManager.GetWorldPosition(Coords.Of(x, y));
+                    var from = new Vector3(cellMid.x,20,cellMid.y);
+                    if(Physics.Raycast(from, Vector3.down, out hit, 30, layerMask))
+                    {
+                        var terrainHeight = Math.Floor(hit.point.y);
+                        worldCells.WriteHeightAt(Coords.Of(x,y), (byte)terrainHeight);
+                    }
+                }
+            }
+        }
+    }
+}
