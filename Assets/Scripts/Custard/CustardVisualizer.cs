40144e5f83c1585f5171080f8ff20cc913c47e1f
Kulesko <thegreg@freenet.de>
2022-05-28 22:20:38 +0200

    fix(custard): corrected custard block height

--- Assets/Scripts/Custard/CustardVisualizer.cs
+++ Assets/Scripts/Custard/CustardVisualizer.cs
@@ -39,8 +39,9 @@ namespace Custard
                     blockGameObject.transform.localScale = new Vector3(scale.x, newCustardLevel, scale.z);
                     var position = blockGameObject.transform.position;
                     // TODO interpolate change
+                    // custard blocks origin point currently is at object center
                     blockGameObject.transform.position = new Vector3(position.x,
-                        worldCells.GetHeightAt(cellUpdate.Coords.X, cellUpdate.Coords.Y) + newCustardLevel - .5f, position.z);
+                        worldCells.GetHeightAt(cellUpdate.Coords.X, cellUpdate.Coords.Y) + newCustardLevel * .5f, position.z);
 
                     custardRenderBlock.Show();
                 }
