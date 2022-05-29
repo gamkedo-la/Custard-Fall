a1e0918d8dc546d0f077f72a058ed79a70e19409
Kulesko <thegreg@freenet.de>
2022-05-29 14:41:05 +0200

    feature(camera): camera follows player in a basic way

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class CameraController : MonoBehaviour
+    {
+        public GameObject followTarget;
+        private Vector3 offset;
+        private double angle;
+
+        private void Start()
+        {
+            var originalTransform = transform;
+            offset = originalTransform.position;
+            angle = originalTransform.rotation.eulerAngles.x;
+        }
+
+        private void FixedUpdate()
+        {
+            transform.position = followTarget.transform.position + offset;
+        }
+    }
+}
