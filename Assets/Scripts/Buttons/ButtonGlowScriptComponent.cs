c87a13cee64aaae6dab046c31e875f2f59b25975
Steven Solof <stebssbets@yahoo.com>
2022-07-17 16:55:41 -0400

    updated UI colors and added glow effect to mouse over on buttons

---
+++ Assets/Scripts/Buttons/ButtonGlowScriptComponent.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonGlowScriptComponent : MonoBehaviour
+{
+    [SerializeField] Image myGlowImage;
+    private Animator myGlowImagesAnimatorComponent;
+
+    private void Awake()
+    {
+        myGlowImagesAnimatorComponent = myGlowImage.GetComponent<Animator>();
+    }
+    public void HandlePointerEnter()
+    {
+        myGlowImagesAnimatorComponent.SetTrigger("MouseEnterGlowTrigger");
+    }
+
+    public void HandlePointerExit()
+    {
+        myGlowImagesAnimatorComponent.SetTrigger("MouseExitDeglowTrigger");
+    }
+}
