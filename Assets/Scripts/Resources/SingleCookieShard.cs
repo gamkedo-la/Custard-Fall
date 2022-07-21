2370db1411827e15103454ec4e3c885dd8a6c208
Kulesko <thegreg@freenet.de>
2022-07-21 13:37:26 +0200

    feature(resources): added single cookie shard

---
+++ Assets/Scripts/Resources/SingleCookieShard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+public class SingleCookieShard : InhaleListener
+{
+    public override void Init()
+    {
+        base.Init();
+
+        AddToInhaleQueue(new Resource("Cookie shard"), .6f);
+    }
+
+    public override void OnResourceInhaled(Inhaler inhaler, Resource resource, int amount)
+    {
+        base.OnResourceInhaled(inhaler, resource, amount);
+        if (GetRemainingResourcesCount() == 0)
+        {
+            gameObject.SetActive(false);
+            Debug.Log("got the cookie shard");
+        }
+    }
+}
