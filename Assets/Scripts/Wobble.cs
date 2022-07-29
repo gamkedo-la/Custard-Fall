9cd6ba36ae9ef6d178bde2eda5f5831e851958e0
Kulesko <thegreg@freenet.de>
2022-07-29 16:26:36 +0200

    feature(resources): resources can wobble with transitional decay

--- Assets/Scripts/Wobble.cs
+++ Assets/Scripts/Wobble.cs
@@ -1,18 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Wobble : MonoBehaviour
 {
+    public float wobbleStrength = 0f;
+    private Vector3 startShakePosition;
+    private float shakeScale = 0.3f;
+    private float decayValue = .95f; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startShakePosition = transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+            transform.position = startShakePosition + Random.insideUnitSphere * (wobbleStrength * shakeScale);
+            wobbleStrength *= decayValue;
     }
 }
