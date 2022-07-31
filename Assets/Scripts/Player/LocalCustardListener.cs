6a765c7430ebd044e1df2c5c28eb847459f611cb
bariskoklu <30669414+bariskoklu@users.noreply.github.com>
2022-07-31 17:13:36 +0300

    Player drowning in the mustard damage is added.

--- Assets/Scripts/Player/LocalCustardListener.cs
+++ Assets/Scripts/Player/LocalCustardListener.cs
@@ -11,6 +11,12 @@ public class LocalCustardListener: MonoBehaviour
     public bool InsideCustard;
     public bool CoveredByCustard;
 
+    public float timeBeforePlayerTakesDamageFromDrawning;
+    public float timeBetweenDamageTicks;
+    public int drowningDamage;
+    private float timePassedSinceCoveredByCustrad = 0.0f;
+    private float timePassedSinceLastDamage = 0.0f;
+
     private void Update()
     {
         var transformPosition = transform.position;
@@ -23,16 +29,41 @@ public class LocalCustardListener: MonoBehaviour
         if (CoveredByCustard != coveredByCustard)
         {
             OnCoveredbyCustard(coveredByCustard);
+            timePassedSinceCoveredByCustrad = 0.0f;
+            timePassedSinceLastDamage = 0.0f;
         }
         
         CoveredByCustard = coveredByCustard;
-        
-        
+
+        CheckForDrownDamage();
     }
 
     private void OnCoveredbyCustard(bool coveredByCustard)
     {
         MusicManager.Instance.SetUnder(coveredByCustard);
     }
-    
+
+    private void CheckForDrownDamage()
+    {
+        if (CoveredByCustard)
+        {
+            if (timePassedSinceCoveredByCustrad < timeBeforePlayerTakesDamageFromDrawning)
+            {
+                timePassedSinceCoveredByCustrad += Time.deltaTime;
+            }
+            else
+            {
+                if (timePassedSinceLastDamage < timeBetweenDamageTicks)
+                {
+                    timePassedSinceLastDamage += Time.deltaTime;
+                }
+                else
+                {
+                    gameObject.GetComponent<Player>().TakeDamage(drowningDamage);
+                    timePassedSinceLastDamage = 0.0f;
+                }
+            }
+        }
+    }
+
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -46,7 +46,7 @@ public class Player : MonoBehaviour
     {
     }
 
-    void TakeDamage(int damage)
+    public void TakeDamage(int damage)
     {
         currentHealth -= damage;
         healthbar.SetHealth(currentHealth);
