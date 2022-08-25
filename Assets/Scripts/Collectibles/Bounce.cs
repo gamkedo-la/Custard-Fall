edd5a8d016743a4a091b05cb8d4f7f91f5f75a71
Taffaz <prog@taffaz.com>
2022-08-25 14:35:10 +0100

    Added sine wave bounce to collectibles

---
+++ Assets/Scripts/Collectibles/Bounce.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+
+    [SerializeField] float amplitude = 0.5f;
+    [SerializeField] float frequency = 1f;
+
+    Vector3 startPos = new Vector3();
+    Vector3 tempPos = new Vector3();
+    float timeOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        timeOffset = UnityEngine.Random.Range(-1f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        tempPos = startPos;
+        tempPos.y += Mathf.Sin ((Time.fixedTime + timeOffset) * Mathf.PI * frequency) * amplitude;
+ 
+        transform.position = tempPos;
+    }
+}
