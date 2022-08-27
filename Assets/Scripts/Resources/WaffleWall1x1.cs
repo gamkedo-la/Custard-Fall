fb0ce7425f48aee93969774fa57ad58933a70213
Kulesko <thegreg@freenet.de>
2022-08-28 01:51:34 +0200

    feature(walls): added waffle wall resource that is an obstacle in world

---
+++ Assets/Scripts/Resources/WaffleWall1x1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+public class WaffleWall1x1 : InhaleListener
+{
+    private WorldCells _worldCells;
+    
+    public override void Init()
+    {
+        base.Init();
+
+        AddToInhaleQueue(new Resource("Waffle wall"), 2f);
+        AddObstacleToWorld(1);
+    }
+
+    private void AddObstacleToWorld(int height)
+    {
+        var position = gameObject.transform.position;
+        var cellPosition = worldCells.GetCellPosition(position.x, position.z);
+        worldCells.WriteWorldItemHeight(cellPosition, worldCells.GetWorldItemHeightAt(cellPosition) + height);
+    }
+
+    public override void OnResourceInhaled(Inhaler inhaler, Resource resource, int amount)
+    {
+        base.OnResourceInhaled(inhaler, resource, amount);
+        if (GetRemainingResourcesCount() == 0)
+        {
+            AddObstacleToWorld(-1);
+            gameObject.SetActive(false);
+            Debug.Log("got the waffle wall piece");
+        }
+    }
+}
