1d8e125424fb97dc754ad01bdb7c273b104938fe
Armando-Navarrete <viking805fan@yahoo.com>
2022-09-22 16:10:29 -0700

    Inventory Icon List Updates - W.I.P.

--- Assets/Scripts/Resources/InventoryUI/InventoryUI.cs
+++ Assets/Scripts/Resources/InventoryUI/InventoryUI.cs
@@ -14,11 +14,20 @@ public class InventoryUI : MonoBehaviour
 	public GameObject inventoryUI;  // The entire UI
 	public Sprite[] iconArt;
 	private InventorySlot[] iconList;
+	private Dictionary<string, int> resnameToIconIndex = new Dictionary<string, int>();
 
 	private bool isShowHideInventoryUi;
 
 	void Start()
 	{
+		resnameToIconIndex["None"] = 0;
+		resnameToIconIndex["Cookie shard"] = 1;
+		//Cookie Mine counts as x5 cookie shards. Not handling yet
+		resnameToIconIndex["Creamy Cone Tree"] = 3;
+		//other tree don't have resource yet
+		resnameToIconIndex["Waffle wall"] = 4; //using tree line
+		resnameToIconIndex["Colletible"] = 6;
+
 		inventory = Inventory.instance;
 		inventory.onItemChangedCallback += UpdateUI;
 		iconList = GetComponentsInChildren<InventorySlot>();
@@ -50,11 +59,14 @@ public class InventoryUI : MonoBehaviour
 		for(int ii = 0; ii < items.Count; ii++)
         {
 			Debug.Log("have:"+items[ii].Name);
+			int iconIndex = resnameToIconIndex[items[ii].Name];
+			Debug.Log("icon " + iconIndex);
+			iconList[ii].AddItem(iconArt[iconIndex]);
         }
-		for (int i = 0; i < iconList.Length; i++)
+		/*for (int i = 0; i < iconList.Length; i++)
         {
 			//To Do: Display Icons Based on inventory, currently just shows putting images on slots
 			iconList[i].AddItem(iconArt[i % iconArt.Length]);
-        }
+        }*/
 	}
 }
