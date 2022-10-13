9d4210756dfacf9844b210a2d92c590b7c464ffb
Kulesko <thegreg@freenet.de>
2022-10-13 21:26:13 +0200

    tweak(resources): small tree gives more resources

--- Assets/Scripts/Resources/SmallCreamyConeTree.cs
+++ Assets/Scripts/Resources/SmallCreamyConeTree.cs
@@ -10,6 +10,9 @@ public class SmallCreamyConeTree : InhaleListener
         base.Init();
 
         AddToInhaleQueue(new Resource("Creamy Cone Tree"), .6f);
+        AddToInhaleQueue(new Resource("Creamy Cone Tree"), .6f);
+        AddToInhaleQueue(new Resource("Creamy Cone Tree"), .6f);
+        AddToInhaleQueue(new Resource("Creamy Cone Tree"), .8f);
     }
 
     public override void OnResourceInhaled(Inhaler inhaler, Resource resource, int amount)
