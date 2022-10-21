de1873dac1125eed4b6b08b19ab113d5813c77e4
Kulesko <thegreg@freenet.de>
2022-10-21 17:24:04 +0200

    tweak(tutorial pins): add a 40% transparency

--- Assets/Scripts/Collectibles/Rotate.cs
+++ Assets/Scripts/Collectibles/Rotate.cs
@@ -16,7 +16,6 @@ public class Rotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
         transform.Rotate(new Vector3(0, speed, 0) * Time.deltaTime);
     }
 }
