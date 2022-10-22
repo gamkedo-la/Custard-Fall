d74cd0870c42b8994fe748d0948aea008f40ca27
Kulesko <thegreg@freenet.de>
2022-10-22 18:04:27 +0200

    asset(glowy orb): introduce glow orb item

---
+++ Assets/Scripts/Resources/GlowOrbItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+public class GlowOrbItem : InhaleListener
+{
+    public override void Init()
+    {
+        base.Init();
+
+        AddToInhaleQueue(new Resource("glow orb"), 1f);
+    }
+    
+}
