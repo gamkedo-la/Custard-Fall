bc3e82b37461f015d7161cef751a5050d9e9cf4a
Kulesko <thegreg@freenet.de>
2022-10-22 18:08:17 +0200

    feature(glow orb): exists only till the morning

--- Assets/Scripts/Resources/GlowOrbItem.cs
+++ Assets/Scripts/Resources/GlowOrbItem.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 
 public class GlowOrbItem : InhaleListener
 {
+    private bool stationary = false;
+    
     public override void Init()
     {
         base.Init();
+
+        // exists only at night
+        TimeManager.onMorningStarted += (sender, arg) =>
+        {
+            if(!stationary)
+                Remove();
+        };
 
         AddToInhaleQueue(new Resource("glow orb"), 1f);
     }
