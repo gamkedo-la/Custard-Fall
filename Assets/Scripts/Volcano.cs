7475ce16ada3aaaa46fd8b24403013e86d4145b3
Kulesko <thegreg@freenet.de>
2022-10-23 15:25:11 +0200

    fix(volcano): do not get stuck in after grappling

--- Assets/Scripts/Volcano.cs
+++ Assets/Scripts/Volcano.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Volcano : MonoBehaviour
+public class Volcano : InhaleListener
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
+    {
+        AddObstacleToWorld(2);
+    }
+
+    private void AddObstacleToWorld(int height)
+    {
+        var position = gameObject.transform.position;
+        var cellPosition = worldCells.GetCellPosition(position.x, position.z);
+        for (int i = -4; i < 4; i++)
+        for (int j = -4; j < 4; j++)
+            // TODO prevent height overflow to heigher places if in corner
+            worldCells.WriteWorldItemHeight(Coords.Of(cellPosition.X + i, cellPosition.Y + j), worldCells.GetWorldItemHeightAt(cellPosition) + height);
+    }
+
+    public override void Init()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnResourceInhaledAndMaybeRemove(Inhaler inhaler, Resource resource, int amount)
     {
-        
+       // do not do anything
     }
 }
