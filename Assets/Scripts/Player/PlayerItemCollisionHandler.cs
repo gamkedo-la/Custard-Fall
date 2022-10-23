447bcb322663e0f41f9f30f6603548621fd19875
Kulesko <thegreg@freenet.de>
2022-10-24 00:57:17 +0200

    fix(item hints): bug stay visible

--- Assets/Scripts/Player/PlayerItemCollisionHandler.cs
+++ Assets/Scripts/Player/PlayerItemCollisionHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -6,6 +7,13 @@ using UnityEngine;
 public class PlayerItemCollisionHandler : MonoBehaviour
 {
     public TextMeshProUGUI textForItemDisplay;
+    private DisappearEffect _disappearEffect;
+    private float _timeSinceActivated;
+
+    private void Awake()
+    {
+        _disappearEffect = textForItemDisplay.GetComponent<DisappearEffect>();
+    }
 
     private void OnTriggerEnter(Collider collision)
     {
@@ -14,7 +22,25 @@ public class PlayerItemCollisionHandler : MonoBehaviour
         {
             textForItemDisplay.enabled = true;
             textForItemDisplay.text = collision.GetComponent<InhaleListener>().interactionMessage;
+            _disappearEffect.Reset();
+            _timeSinceActivated = Time.time;
+            StartCoroutine(DisappearLater());
         }
+    }
+
+    private IEnumerator DisappearLater()
+    {
+        var sinceActivated = _timeSinceActivated;
+        yield return new WaitForSecondsRealtime(3f);
+
+        // target did not switch meanwhile
+        if (Math.Abs(sinceActivated - _timeSinceActivated) < .1f)
+            Hide();
+    }
+
+    private void Hide()
+    {
+        _disappearEffect.Activate();
     }
 
     private void OnTriggerExit(Collider collision)
@@ -22,7 +48,7 @@ public class PlayerItemCollisionHandler : MonoBehaviour
         collision.GetComponent<InhaleListener>();
         if (collision.GetComponent<InhaleListener>() != null)
         {
-            textForItemDisplay.enabled = false;
+            Hide();
         }
     }
 }
