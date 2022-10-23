a2343905e107e95195c1487fefeb43bd0c96452f
Kulesko <thegreg@freenet.de>
2022-10-24 00:48:28 +0200

    fix(item hints): stay visible

---
+++ Assets/DisappearEffect.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DisappearEffect : MonoBehaviour
+{
+    private float _targetAlpha;
+    private TextMeshProUGUI _textMesh;
+    private Color _color;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _textMesh = GetComponent<TextMeshProUGUI>();
+        _color = _textMesh.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Activate()
+    {
+        _targetAlpha = 0;
+        _textMesh.CrossFadeAlpha(_targetAlpha, .22f, true);
+    }
+
+    public void Reset()
+    {
+        _targetAlpha = 1;
+        _textMesh.color = _color;
+        Debug.Log(_color.a);
+        _textMesh.CrossFadeAlpha(_color.a,0,true);
+    }
+}
