b3f99bbda5d61ecefc9b37a8997707b028b119ff
Kulesko <thegreg@freenet.de>
2023-02-13 21:05:04 +0100

    feature(craftable items): initial commit of ScriptableObjects

---
+++ Assets/Scripts/Crafting/CraftableItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "CustardFall/CraftableItem")]
+public class CraftableItem : ScriptableObject
+{
+    [SerializeField] private PlaceableItem item;
+
+
+    [SerializeField] private List<CraftingRequirement> requirements;
+
+    public PlaceableItem Item => item;
+
+    [Serializable]
+    public class CraftingRequirement
+    {
+        [SerializeField]
+        private string resourceName;
+
+        [SerializeField]
+        private int amount;
+    }
+}
