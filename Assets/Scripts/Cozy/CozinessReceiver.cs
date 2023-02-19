898c652e3d54ee4934150306ddec3572f6063081
Kulesko <thegreg@freenet.de>
2023-02-19 22:10:55 +0100

    feature(feeling cozy): player feels increasingly cozy in camp up to max possible level related to how cozy the camp actually is

--- Assets/Scripts/Cozy/CozinessReceiver.cs
+++ Assets/Scripts/Cozy/CozinessReceiver.cs
@@ -1,11 +1,16 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class CozinessReceiver : MonoBehaviour
 {
 
     [SerializeField] private float cozyLevel;
+    [SerializeField] private float feelingCozy = 0;
+    [SerializeField] private float baseFillDuration = 15f;
+    
 
     public float CozyLevel => cozyLevel;
+    public float DeelingCozy => feelingCozy;
 
     public void OnCozyReceive(float coziness)
     {
@@ -18,6 +23,20 @@ public class CozinessReceiver : MonoBehaviour
         if (cozyLevel < 0)
         {
             cozyLevel = 0;
+        }
+    }
+
+    private void Update()
+    {
+        var maxValue = Mathf.Floor(cozyLevel);
+        Debug.Log(maxValue);
+        if (feelingCozy < maxValue)
+        {
+            feelingCozy += maxValue/baseFillDuration * Time.deltaTime;
+        }
+        else
+        {
+            feelingCozy = maxValue;
         }
     }
 }
