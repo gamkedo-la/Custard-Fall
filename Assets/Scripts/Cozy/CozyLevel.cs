63b56a16d63d4614ce9da712f0c390844cf8b986
Kulesko <thegreg@freenet.de>
2023-02-25 21:34:19 +0100

    feature(cozy levels): ScriptableObjects for levels of coziness

---
+++ Assets/Scripts/Cozy/CozyLevel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+
+    [CreateAssetMenu(menuName = "CustardFall/CozyLevelOfSurrounding")]
+    public class CozyLevel : ScriptableObject
+    {
+        [SerializeField] private string displayName;
+        [SerializeField] private Color color;
+        [SerializeField] private Color progressColor;
+
+        public string DisplayName => displayName;
+        public Color Color => color;
+        public Color ProgressColor => progressColor;
+        
+    }
--- Assets/Scripts/Cozy/CozySettings.cs
+++ Assets/Scripts/Cozy/CozySettings.cs
@@ -1,10 +1,14 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 [CreateAssetMenu(menuName = "CustardFall/CozySettings")]
 public class CozySettings : ScriptableObject
 {
     [SerializeField] private AnimationCurve easingFunction;
+    [SerializeField] private List<CozyLevel> levels;
 
     public AnimationCurve EasingFunction => easingFunction;
+    public List<CozyLevel> Levels => levels;
+
 }
