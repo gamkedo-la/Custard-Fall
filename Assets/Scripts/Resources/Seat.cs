ccf2ef2217b324ba15f06cd2ed21e6e022d70611
Kulesko <thegreg@freenet.de>
2023-02-27 07:34:50 +0100

    asset(seat): add special items to find - seat red and gray

---
+++ Assets/Scripts/Resources/Seat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+public class Seat : Inhalable
+{
+    
+    private PlaceableItem _placeableItem;
+    
+    public override void Init()
+    {
+        base.Init();
+
+        AddToInhaleQueue(new Resource("Seat", _placeableItem), 1.5f);
+    }
+
+    private void Awake()
+    {
+        _placeableItem = GetComponent<PlaceableItemReference>().Item();
+    }
+}
