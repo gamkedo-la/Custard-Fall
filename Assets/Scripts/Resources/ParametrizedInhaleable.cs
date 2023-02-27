cc1998d448363404f5200f6052306dac0b10315a
Kulesko <thegreg@freenet.de>
2023-02-27 22:18:33 +0100

    tweak(cozy levels): rename cozy levels to snug levels

---
+++ Assets/Scripts/Resources/ParametrizedInhaleable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+
+[RequireComponent(typeof(PlaceableItemReference))]
+public class ParametrizedInhaleable : Inhalable
+{
+    [SerializeField] private string resourceName;
+    
+    private PlaceableItem _placeableItem;
+    
+    public override void Init()
+    {
+        base.Init();
+
+        AddToInhaleQueue(new Resource(resourceName, _placeableItem), 1.5f);
+    }
+
+    private void Awake()
+    {
+        _placeableItem = GetComponent<PlaceableItemReference>().Item();
+    }
+}
