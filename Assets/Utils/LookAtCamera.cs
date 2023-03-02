9079bf3caad5c9743448e12a3571d8c778e9349c
Kulesko <thegreg@freenet.de>
2023-03-02 15:32:00 +0100

    feature(utils): add script to make a sprite always look at the main camera

---
+++ Assets/Utils/LookAtCamera.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+
+    public class LookAtCamera : MonoBehaviour
+    {
+        private void Update()
+        {
+            var transformRotation = this.transform.rotation;
+            this.transform.LookAt(Camera.main.transform);
+        }
+    }
