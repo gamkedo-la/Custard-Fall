04f17ebc5120ee6e928678aff7f7f29c73665b53
Kulesko <thegreg@freenet.de>
2023-03-05 09:06:53 +0100

    fix(upgradable items): make upgrade visual look at the camera

--- Assets/Utils/LookAtCamera.cs
+++ Assets/Utils/LookAtCamera.cs
@@ -4,9 +4,28 @@ using UnityEngine;
 
     public class LookAtCamera : MonoBehaviour
     {
-        private void Update()
+
+        private enum Mode
         {
-            var transformRotation = this.transform.rotation;
-            this.transform.LookAt(Camera.main.transform);
+            LookAtCamera,
+            LookAtCameraInverted,
+        }
+
+        [SerializeField] private Mode lookAtMode;
+    
+        private void LateUpdate()
+        {
+            switch (lookAtMode)
+            {
+                case Mode.LookAtCamera:
+                    this.transform.forward = Camera.main.transform.forward;
+                    break;
+                case Mode.LookAtCameraInverted:
+                    this.transform.forward = -Camera.main.transform.forward;
+                    break;
+                default:
+                    this.transform.LookAt(Camera.main.transform);
+                    break;
+            }
         }
     }
