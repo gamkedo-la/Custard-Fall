2750ed2ee20b2e4edcffd7d6050cbc53d0ed61d4
PatThompson88 <patrick.thompson688@gmail.com>
2023-06-04 19:11:23 -0400

    Adds script to trigger character animator

---
+++ Assets/Scripts/Player/AnimationScript.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationScript : MonoBehaviour
+{
+    // handle changing the character's animation state
+}
