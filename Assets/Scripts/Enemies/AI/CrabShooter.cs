17b53aa96453999a73801cdcf06bbc6d207dc85b
Kulesko <thegreg@freenet.de>
2023-08-08 16:30:40 +0200

    feature(enemy): distance aware crab shooter

---
+++ Assets/Scripts/Enemies/AI/CrabShooter.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(LookAtObject))]
+[RequireComponent(typeof(ProjectileSpawner))]
+public class CrabShooter : Mob
+{
+
+    protected LookAtObject lookAtObject;
+    protected ProjectileSpawner projectileSpawner;
+    
+    protected override void Awake()
+    {
+        base.Awake();
+        lookAtObject = GetComponent<LookAtObject>();
+        projectileSpawner = GetComponent<ProjectileSpawner>();
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        _tracker.onTargetEnter += MaybeGetAngry;
+        _tracker.onTargetExit += MaybeCalmDown;
+    }
+    
+    protected override void MaybeCalmDown()
+    {
+        projectileSpawner.enabled = false;
+        lookAtObject.enabled = false;
+    }
+
+    protected override void MaybeGetAngry()
+    {
+        projectileSpawner.enabled = true;
+        lookAtObject.enabled = true;
+    }
+}
---
+++ Assets/Scripts/Enemies/AI/Mob.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(DistanceTracker))]
+public class Mob : MonoBehaviour
+{
+    protected DistanceTracker _tracker;
+
+    protected virtual void Awake()
+    {
+        _tracker = GetComponent<DistanceTracker>();
+    }
+
+    protected virtual void Start()
+    {
+        _tracker.onTargetEnter += MaybeGetAngry;
+        _tracker.onTargetExit += MaybeCalmDown;
+    }
+
+    protected virtual void MaybeCalmDown()
+    {
+        Debug.Log("calming down");
+    }
+
+    protected virtual void MaybeGetAngry()
+    {
+        Debug.Log("getting angry");
+    }
+}
