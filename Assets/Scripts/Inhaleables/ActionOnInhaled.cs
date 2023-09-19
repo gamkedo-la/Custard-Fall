30502ebe8ed1fe2426631fa44942b18d6548d731
Kulesko <thegreg@freenet.de>
2023-09-19 22:39:14 +0200

    feature(radiance orbs): inhaling radiance orbs gives a radiance bonus

---
+++ Assets/Scripts/Inhaleables/ActionOnInhaled.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+
+public class ActionOnInhaled : MonoBehaviour
+{
+
+    private Inhalable _inhalable;
+    [SerializeField]
+    private float radianceBonus;
+    
+    private void Awake()
+    {
+        _inhalable = gameObject.GetComponent<Inhalable>();
+        _inhalable.onInhaled += IncreaseRadiance;
+    }
+
+    private void IncreaseRadiance(Inhaler inhaler, Resource resource, int amount)
+    {
+        var radianceReceiver = inhaler.gameObject.GetComponentInParent<CozinessReceiver>();
+        Debug.Log($"inhaled by player {radianceReceiver != null}");
+        if (radianceReceiver != null)
+        {
+            radianceReceiver.IncreaseRadiance(radianceBonus);
+        }
+    }
+    
+}
