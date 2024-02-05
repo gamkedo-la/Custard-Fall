07ff6c52e2f9adaa3c382667ce6c497d3ce800ca
Kulesko <thegreg@freenet.de>
2024-02-05 19:34:03 +0100

    feature(orbs): inhaled orbs can restore health

--- Assets/Scripts/Inhaleables/ActionOnInhaled.cs
+++ Assets/Scripts/Inhaleables/ActionOnInhaled.cs
@@ -2,11 +2,10 @@
 
 public class ActionOnInhaled : MonoBehaviour
 {
-
     private Inhalable _inhalable;
-    [SerializeField]
-    private float radianceBonus;
-    
+    [SerializeField] private float radianceBonus = 0;
+    [SerializeField] private int healthBonus = 0;
+
     private void Awake()
     {
         _inhalable = gameObject.GetComponent<Inhalable>();
@@ -15,11 +14,22 @@ public class ActionOnInhaled : MonoBehaviour
 
     private void IncreaseRadiance(Inhaler inhaler, Resource resource, int amount)
     {
-        var radianceReceiver = inhaler.gameObject.GetComponentInParent<RadianceReceiver>();
-        if (radianceReceiver != null)
+        if (radianceBonus != 0f)
         {
-            radianceReceiver.IncreaseRadiance(radianceBonus);
+            var radianceReceiver = inhaler.gameObject.GetComponentInParent<RadianceReceiver>();
+            if (radianceReceiver != null)
+            {
+                radianceReceiver.IncreaseRadiance(radianceBonus);
+            }
+        }
+
+        if (healthBonus != 0)
+        {
+            var player = inhaler.gameObject.GetComponentInParent<Player>();
+            if (player != null)
+            {
+                player.TakeDamage(-healthBonus, DamageImplication.Health);
+            }
         }
     }
-    
 }
