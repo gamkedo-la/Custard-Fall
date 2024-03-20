a5b0be761f80350b5f5eaef5178483a6e77c9045
Kulesko <thegreg@freenet.de>
2024-03-21 00:26:50 +0100

    prepare short lived vfx spawn object

---
+++ Assets/Scripts/Utils/VfxInstance.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+
+public class VfxInstance : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem vfx;
+
+    private void Start()
+    {
+        vfx.gameObject.SetActive(true);
+    }
+
+    private void FixedUpdate()
+    {
+        if (vfx.isStopped)
+        {
+            Destroy(this);
+        }
+    }
+}
