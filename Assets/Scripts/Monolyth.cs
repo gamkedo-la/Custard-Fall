1021a1251a794ddba162259bc61508020fcbec48
Kulesko <thegreg@freenet.de>
2024-03-29 12:05:27 +0100

    feature(vfx): add wave effect to when the player activates the monolyth

--- Assets/Scripts/Monolyth.cs
+++ Assets/Scripts/Monolyth.cs
@@ -4,6 +4,10 @@ public class Monolyth : MonoBehaviour
 {
     private UpgradeableStructure _upgrader;
     private Tidesmanager _tidesmanager;
+
+    [SerializeField] private VfxInstance vfxOnActivated;
+    [SerializeField] private Transform vfxSpawn;
+    
     void Awake()
     {
         _upgrader = GetComponent<UpgradeableStructure>();
@@ -12,8 +16,16 @@ public class Monolyth : MonoBehaviour
         _tidesmanager = FindObjectOfType<Tidesmanager>();
     }
 
+    [ContextMenu("VFX")]
+    public void PlayVFX()
+    {
+        if (vfxOnActivated)
+            VfxInstance.Spawn(vfxOnActivated, vfxSpawn.position,Quaternion.identity);
+    }
+
     private void ShrinkCustard(object sender, UpgradeableStructure.UpgradeArgs e)
     {
+        PlayVFX();
         var tidesmanagerIndexOfCurrentDayTimeTideLevel = _tidesmanager.indexOfCurrentDayTimeTideLevel - 1;
         if (tidesmanagerIndexOfCurrentDayTimeTideLevel < 0)
         {
